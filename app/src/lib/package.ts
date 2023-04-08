789d7c146ba266c8ac57f5afe62d8d7250e68b56
Frank Steiler <frank@steilerdev.de>
2023-04-08 18:58:28 +0000

    testing

---
+++ app/src/lib/package.ts
@@ -0,0 +1,5 @@
+// Exposing package metadata to the application
+export const VERSION = `1.2.0-nightly`;
+export const BUILD_NUMBER = ''
+export const NAME = `icloud-photos-sync`;
+export const DESC = `One-way sync engine for iCloud Photos Library within the native file system`;
