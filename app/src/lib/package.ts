2279176229f663f81f398485b3ee503497211b10
steilerDev <steilerDev@users.noreply.github.com>
2023-04-10 19:01:09 +0000

    Release bump to 1.2.0-beta.8

--- app/src/lib/package.ts
+++ app/src/lib/package.ts
@@ -1,4 +1,4 @@
 // Exposing package metadata to the application
-export const VERSION = `1.2.0-beta.7`;
+export const VERSION = `1.2.0-beta.8`;
 export const NAME = `icloud-photos-sync`;
 export const DESC = `One-way sync engine for iCloud Photos Library within the native file system`;
